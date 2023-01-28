fcc94477eff016af64dd5f16beff287f82dcf7e6
Daniel Canuto <danielpscanuto83@gmail.com>
2023-01-28 15:47:51 -0300

    criando partials e base home

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from home.views import IndexView
+from home.views import SobreView
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
+    path('sobre/', SobreView.as_view(), name="sobre"),
 ]
--- home/views.py
+++ home/views.py
@@ -2,4 +2,7 @@ from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
-    template_name = "index.html"
+    template_name = "home/index.html"
+
+class SobreView(TemplateView):
+    template_name = "home/sobre.html"
